a50a583634f53e0adb24e9cd6f4d5eb68978d631
MiMi-Yup <tuivanthiet@gmail.com>
2024-06-29 15:35:35 +0700

    (...) Move to new domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://vchat-app.azurewebsites.net/api/",
-  chatHub: "https://vchat-app.azurewebsites.net/chatHub/"
+  apiUrl: "https://vchat2.azurewebsites.net/api/",
+  chatHub: "https://vchat2.azurewebsites.net/chatHub/"
 };
